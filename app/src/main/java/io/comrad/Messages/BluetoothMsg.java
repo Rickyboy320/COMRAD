57df75bdd3fa8f2f77ef5e9c8bf231714d7f5d16
Wouter Loeve <wouter.loeve@protonmail.com>
2019-01-10 17:07:08 +0100

    Starting building the message protocol

---
+++ app/src/main/java/io/comrad/Messages/BluetoothMsg.java
@@ -0,0 +1,67 @@
+package io.comrad.Messages;
+
+import android.bluetooth.BluetoothDevice;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import io.comrad.R;
+
+public class BluetoothMsg {
+    BluetoothDevice destination;
+    ArrayList<BluetoothDevice> route;
+    MessageType type;
+    byte[] payload;
+
+    // Use super?
+    BluetoothMsg(BluetoothDevice destination, ArrayList<BluetoothDevice> route,
+                 MessageType type) {
+        this.destination = destination;
+        this.route = route;
+        this.type = type;
+    }
+
+    BluetoothMsg(BluetoothDevice destination, ArrayList<BluetoothDevice> route,
+                 MessageType type, byte[] payload) {
+        this.destination = destination;
+        this.route = route;
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public byte[] toByteArray() throws IOException {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        ObjectOutputStream objStream = new ObjectOutputStream(byteStream);
+        objStream.writeObject(this);
+
+        return byteStream.toByteArray();
+    }
+
+    public void createByteArayFromAudioFile(String fileURI) throws IOException {
+        try {
+            FileOutputStream fileoutputstream = new FileOutputStream(fileURI);
+            fileoutputstream.write(payload);
+            fileoutputstream.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void add_payload(Object message) {
+        switch(this.type){
+            case routing :
+
+                break;
+            case playlist:
+                break;
+            case song:
+
+
+        }
+    }
+
+
+}
