8300d8b16bf3db8d336e96dd23371c48b9d9a77b
Wouter Loeve <wouter.loeve@protonmail.com>
2019-01-24 15:45:05 +0100

    Yolo

--- app/src/main/java/io/comrad/music/PlayMusic.java
+++ app/src/main/java/io/comrad/music/PlayMusic.java
@@ -1,6 +1,7 @@
 package io.comrad.music;
 
 import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import io.comrad.R;
 
 import static android.content.ContentValues.TAG;
@@ -17,16 +24,18 @@ import static android.content.ContentValues.TAG;
 
 public class PlayMusic extends Fragment  {
 
-    private AudioTrack audioTrack;
+//    private AudioTrack audioTrack;
     private ImageButton playButton;
+    private MediaPlayer currentMediaPlayer = new MediaPlayer();
+    private MediaPlayer nextMediaPlayer = new MediaPlayer();
 
     private void playCurrentSong() {
-        if (audioTrack.getPlayState() == AudioTrack.PLAYSTATE_PLAYING) {
-            audioTrack.pause();
+        if (currentMediaPlayer.isPlaying()) {
+            currentMediaPlayer.pause();
             playButton.setImageResource(android.R.drawable.ic_media_play);
 
         } else {
-            audioTrack.play();
+            currentMediaPlayer.start();
             playButton.setImageResource(android.R.drawable.ic_media_pause);
         }
     }
@@ -40,7 +49,6 @@ public class PlayMusic extends Fragment  {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View playmusic = inflater.inflate(R.layout.fragment_play_music, container, false);
-//        ImageView img = (ImageView)playmusic.findViewById(R.id.playIcon);
         playButton = playmusic.findViewById(R.id.playButton);
         playButton.setImageResource(android.R.drawable.ic_media_play);
         playButton.setOnClickListener(new View.OnClickListener() {
@@ -52,15 +60,50 @@ public class PlayMusic extends Fragment  {
         return playmusic;
     }
 
-    public void setAudioTrack(AudioTrack audioTrack) {
-        this.audioTrack = audioTrack;
-        this.audioTrack.play();
+    private void prepareNextMediaPlayer(byte[] mp3SoundByteArray, MediaPlayer mediaPlayer) {
+        try {
+            /* create temp file that will hold byte array */
+            File tempMp3 = File.createTempFile("tmpSong", "mp3", getActivity().getCacheDir());
+            tempMp3.deleteOnExit();
+            FileOutputStream fos = new FileOutputStream(tempMp3);
+            fos.write(mp3SoundByteArray);
+            fos.flush();
+            fos.close();
+
+            // resetting mediaplayer instance to evade problems
+            mediaPlayer.reset();
+            Log.d(TAG, "!~! currentMediaPlayer has been reset!");
+
+            FileInputStream inputStream = new FileInputStream(tempMp3);
+
+            mediaPlayer.setDataSource(inputStream.getFD(), 0, mp3SoundByteArray.length);
+
+            inputStream.close();
+
+            Log.d(TAG, "!~! currentMediaPlayer source has been set!");
+
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer currentMediaPlayer) {
+                    currentMediaPlayer.start();
+                    playButton.setImageResource(android.R.drawable.ic_media_pause);
+                }
+            });
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void switchMediaPlayer() {
+        MediaPlayer temp = currentMediaPlayer;
+        currentMediaPlayer = nextMediaPlayer;
+        nextMediaPlayer = currentMediaPlayer;
     }
 
     public void incrementProgress(int size, int diff) {
         ProgressBar progressbar = getActivity().findViewById(R.id.progressBar);
         double percentage =  (float)diff / (float)size * 100;
-        System.out.println("LALALALAL" + diff + " " + size);
         progressbar.setProgress((int) Math.round(percentage));
     }
 }
